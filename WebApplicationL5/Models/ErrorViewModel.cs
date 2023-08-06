fab5ee4c21c3620b01d1a476960d9550a340154c
Константин Константин <kos2akb@gmail.com>
2023-08-07 00:55:10 +0300

    CRUD works

--- WebApplicationL5/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace WebApplicationL5.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- WebApplicationL5/Models/Product.cs
+++ WebApplicationL5/Models/Product.cs
@@ -3,8 +3,9 @@ namespace WebApplicationL5.Models;
 public class Product
 {
     public int Id { get; set; }
-    public string? Name { get; set; }
-    public string? Description { get; set; }
+    public string? Name { get; set; } 
+    public string? Description { get; set; } 
     public double Price { get; set; }
     public int Amount { get; set; }
+    
 }
