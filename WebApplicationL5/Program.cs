e8625a4090cef4f3dd4f8859a27c4394733f81c8
Константин Константин <kos2akb@gmail.com>
2023-08-08 00:47:03 +0300

    added policy and json converter

--- WebApplicationL5/Program.cs
+++ WebApplicationL5/Program.cs
@@ -1,7 +1,24 @@
+//using WebApplicationL5.JsonSettings.Converters;
+using WebApplicationL5.JsonSettings.Policies;
+using System.Text.Json.Serialization;
+using WebApplicationL5.JsonSettings.Converters;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews()
+    .AddRazorRuntimeCompilation()
+    .AddJsonOptions(options =>
+    {
+        //options.JsonSerializerOptions.PropertyNamingPolicy = new SnakeCaseNamePolicy();
+       options.JsonSerializerOptions.NumberHandling = JsonNumberHandling.AllowReadingFromString | JsonNumberHandling.AllowNamedFloatingPointLiterals;
+       options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+       options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+       options.JsonSerializerOptions.Converters.Add(new DoubleConverter());
+    });
+
+
+    builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -11,6 +28,12 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
